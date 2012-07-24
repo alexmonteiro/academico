229fc63eec7cd519bbf6b57382e7977de3658ab8
hylozero <hylozero@gmail.com>
2012-07-24 14:05:20 -0300

    Welcome Controller Criada

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 AcademicoRails::Application.routes.draw do
+  get "welcome/index"
+
   resources :depts
 
   resources :dept_types
@@ -75,7 +77,7 @@ AcademicoRails::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'welcome#index'
 
   # See how all your routes lay out with "rake routes"
 
