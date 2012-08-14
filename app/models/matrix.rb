a7b313e6754ed5cd5244c40c74cbb54d9b573d20
alexmonteiro <alexmonteiro@gmail.com>
2012-08-14 18:15:53 -0300

    alteração dos menus e tradução das estrutura

--- app/models/matrix.rb
+++ app/models/matrix.rb
@@ -4,4 +4,8 @@ class Matrix < ActiveRecord::Base
   belongs_to :matrix_status
   belongs_to :class_season_type
   attr_accessible :ended_at, :maxdisciplines, :maxseasons, :started_at
+
+  def model_custom_name
+       self.id.to_s+ '/' + self.course.name + ' - '  + self.matrix_evaluation_type.description
+  end  
 end
