0ef42866ee9d2ccd838f6d10954d4fcc3f7bb85d
alexmonteiro <alexmonteiro@gmail.com>
2012-09-04 14:53:30 -0300

    correção no cadastro de disciplina.
    A inclusão de curso e disciplina aceitou o cadastro, porém o código
    deve ser gerado automaticamente pelo sistema em ambos e a forma de
    criação da disciplina, já que está dentro do curso, não é necessário
    sinalizar qual é a unidade organizacional a qual pertence (esta será a
    mesma do curso)

--- app/models/discipline.rb
+++ app/models/discipline.rb
@@ -1,9 +1,8 @@
 class Discipline < ActiveRecord::Base
   belongs_to :discipline_type
   belongs_to :course
-  belongs_to :dept
-  attr_accessible :code, :credits, :description, :goals, :name, :workload, :discipline_type_id, :course_id, :dept_id, :discipline_type_id, :goals
+  attr_accessible :code, :credits, :description, :goals, :name, :workload, :discipline_type_id, :course_id, :discipline_type_id, :goals
   
-  validates_presence_of :name, :course_id, :discipline_type_id, :dept_id
+  validates_presence_of :name, :course_id, :discipline_type_id
   
 end
--- db/migrate/20120808192541_create_disciplines.rb
+++ db/migrate/20120808192541_create_disciplines.rb
@@ -9,12 +9,10 @@ class CreateDisciplines < ActiveRecord::Migration
       t.integer :credits
       t.references :discipline_type
       t.references :course
-      t.references :dept
 
       t.timestamps
     end
     add_index :disciplines, :discipline_type_id
     add_index :disciplines, :course_id
-    add_index :disciplines, :dept_id
   end
 end
