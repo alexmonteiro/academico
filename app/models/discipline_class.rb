f9492d557087cbe1813c3828c75b23e7204d440c
alexmonteiro <alexmonteiro@gmail.com>
2012-09-04 14:20:15 -0300

    associacao docencia e classe

--- app/models/discipline_class.rb
+++ app/models/discipline_class.rb
@@ -1,6 +1,7 @@
 class DisciplineClass < ActiveRecord::Base
   belongs_to :school_class
   belongs_to :matrix_discipline
+  has_many :class_teachings, :foreign_key => "discipline_class_id"
   attr_accessible :code, :ending_at, :started_at, :timetable_started_at, :vancancies, :school_class_id, :matrix_discipline_id
   
   validates_uniqueness_of :school_class_id, :scope => :matrix_discipline_id, :message => "já possui esta disciplina associada a classe."
