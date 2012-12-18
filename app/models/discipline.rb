16b8120b9c562311c1c75a706d44ae8725f36df5
alexmonteiro <alexmonteiro@gmail.com>
2012-12-18 10:31:00 -0200

    controle de exclusao de disciplinas vinculadas a matriz

--- app/models/discipline.rb
+++ app/models/discipline.rb
@@ -1,9 +1,24 @@
 class Discipline < ActiveRecord::Base
   belongs_to :discipline_type
   belongs_to :course
+  has_many :matrix_disciplines
   attr_accessible :code, :credits, :description, :goals, :name, :workload, :discipline_type_id, :course_id, :discipline_type_id
   default_scope :order => :name
+  before_destroy :has_children?
   
   validates_presence_of :name, :course_id, :discipline_type_id
+  
+
+
+private
+  def has_children?
+    errors.add(:base, "Existem Matrizes associadas a esta Disciplina") unless matrix_disciplines.count == 0
+   
+    if errors.size > 0
+     false
+    else
+     true
+    end
+  end
   
 end
