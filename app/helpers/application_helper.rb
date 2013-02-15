f6c0f0421ab00f16e636b93f88666871b5673294
Tiago da Silva Valeriano de Oliveira <tiago.svo@gmail.com>
2013-02-15 11:29:55 -0200

    Comitando a Migration Person_Id

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -35,6 +35,20 @@ module ApplicationHelper
     end
   end
   
+  #Metodos para os campos aninhados
+  def link_to_remove_fields(name, f)
+    f.hidden_field(:_destroy) + link_to_function(name, "remove_fields(this)")
+  end
+  
+  def link_to_add_fields(name, f, association)
+    new_object = f.object.class.reflect_on_association(association).klass.new
+    fields = f.fields_for(association, new_object, :child_index => "new_#{association}") do |builder|
+      render(association.to_s.singularize + "_fields", :f => builder)
+    end
+    link_to_function(name,"add_fields(this, \"#{association}\", \"#{escape_javascript(fields)}\")")
+  end
+  #Fim dos metodos para campos aninhados
+  
   #Metodo para gerar o Menu de Configurações
   def menu_config
     #Array para determinar as controllers desejadas e grupos
