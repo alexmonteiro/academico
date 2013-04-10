b2d2ecad1b7cf305a58a137a37eec148b616fb3f
alexmonteiro <alexmonteiro@gmail.com>
2013-04-10 15:46:40 -0300

    grade horária e registro de aulas com vários horários.

--- app/helpers/class_records_helper.rb
+++ app/helpers/class_records_helper.rb
@@ -1,2 +1,6 @@
 module ClassRecordsHelper
+  # verifica se horario da aula jah foi inserido no dia
+  def check_class_time(discipline_class_id, class_time_id, recorded_at)
+    ClassRecord.find_by_discipline_class_id_and_class_time_id_and_recorded_at(discipline_class_id,class_time_id, recorded_at) ? true : false
+  end
 end
