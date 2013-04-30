e2841bdac0c820d9cd2822f1258d63ae597b0555
Tiago da Silva Valeriano de Oliveira <tiago.svo@gmail.com>
2013-04-30 11:37:35 -0300

    Correção do Vinculo Institucional

--- app/models/person.rb
+++ app/models/person.rb
@@ -53,48 +53,57 @@ class Person < ActiveRecord::Base
     string :name    
   end  
   
-  #Metodo para salvar os Person_Person_Type_Depts
+  #Metodo para Salvar os campos de Person_Person_Type_Depts em Person
   def create_depts_by_person_types(params)
     person_types_hash = params[:person_type_depts_attributes] #=> Depts
     person_types_checkeds = params[:person_types_checkeds] #=> Check_box checkeds
-    if !person_types_checkeds.blank?
-      person_types_checkeds.each do |ptc|
-        if ptc != "1"
-          person_person_type_dept = self.person_person_types.build(:person_type_id => ptc)
-          person_person_type_dept.save!
-          if !person_types_hash[ptc].blank?
-            !person_types_hash[ptc].each do |pt|
-              person_person_type_dept.person_person_type_depts.build(:dept_id => pt).save! if !pt.blank?
+    if !person_types_checkeds.blank? # => Verifica se o hash de Check_boxes está vazio (sem nenhum checked)
+      person_types_checkeds.each do |ptc| # => Realiza uma iteração para cada Vinculo Institucional Checked
+        if ptc != "1" # => No caso do id 1, este está relacionado ao tipo de Vinculo Intitucioinal `Aluno` ao qual não está ligado a nenhuma Unidade Organizacional pois este se vincula através da Matrícula
+          person_person_type_dept = self.person_person_types.build(:person_type_id => ptc) # => Aqui cria-se o objeto person_person_type para pessoas com o person_type com o valor que a variavel 'ptc' oferecer
+          person_person_type_dept.save! # => Salva o objeto criado na na linha de cima
+          if !person_types_hash[ptc].blank? # => Verifica se o Vinculo Institucional trazido está vazio no hash de Depts de Unidade Organizacional (exceto no caso de 'Aluno')
+            person_types_hash[ptc].each do |pt| # => Iteração sobre cada Vinculo Institucional para pegar as Unidades Organizacionais Vinculadas
+              person_person_type_dept.person_person_type_depts.build(:dept_id => pt).save! if !pt.blank? # => Aqui cria-se o objeto person_person_type_depts para pessoas com o person_type com o valor que a variavel 'pt' oferecer e salva
             end
           end
-        else
-          person_person_type_dept = self.person_person_types.build(:person_type_id => ptc)
-          person_person_type_dept.save!
+        else # => Se for id 1 do tipo Aluno
+          person_person_type_dept = self.person_person_types.build(:person_type_id => ptc) # => Aqui cria-se o objeto person_person_type para pessoas com o person_type com o valor que a variavel '1' para aluno
+          person_person_type_dept.save! # => Salva o objeto criado na na linha de cima
         end
       end
     end
     
   end
   
+  #Metodo para Validar os campos de Person_Person_Type_Depts em Person
   def validates_depts_by_person_types(params)
-    person_types_hash = params[:person_type_depts_attributes] #=> Depts
-    person_types_checkeds = params[:person_types_checkeds] #=> Check_box checkeds
+    person_types_hash = params[:person_type_depts_attributes] #=> Hash de Depts indexados por tipo de pessoa
+    person_types_checkeds = params[:person_types_checkeds] #=> Hash de Check_box checkeds
     
-    if person_types_checkeds.blank?
-      errors.add(:base, "Cadastro precisa ter algum Vínculo Institucional")
-      return false
-    else
-      validator = true
-      person_types_checkeds.each do |ptc|
-        if ptc != "1"
-          if person_types_hash[ptc].blank?
-            errors.add(:base, "O Vinculo Institucional '#{PersonType.find(ptc).description}' tem que estar ligado a um Instituto")
-            validator = false
+    if person_types_checkeds.blank? # => Verifica se o hash de Check_boxes está vazio (sem nenhum checked)
+      errors.add(:base, "Cadastro precisa ter algum Vínculo Institucional") # => Adiciona o erro
+      validator = false # => retorna falso se nao houver nenhum Vinculo Institucional
+    else 
+      validator = true # => retorna verdadeiro por um momento se as condições seguintes não forem atendidas
+      person_types_checkeds.each do |ptc| # => Realiza uma iteração para cada Vinculo Institucional Checked
+        if ptc != "1" # => No caso do id 1, este está relacionado ao tipo de Vinculo Intitucioinal `Aluno` ao qual não está ligado a nenhuma Unidade Organizacional pois este se vincula através da Matrícula
+          if person_types_hash[ptc].blank? # => Verifica se o Vinculo Institucional trazido está vazio no hash de Depts de Unidade Organizacional (exceto no caso de 'Aluno')
+            errors.add(:base, "O Vinculo Institucional '#{PersonType.find(ptc).description}' tem que estar ligado a uma Unidade Organizacional") # => Adiciona o erro
+            validator = false # => retorna falso se nao houver nenhuma Unidade Organizacional selecionada se o Vinculo Institucional estiver selecionado
             break
+          else
+            person_types_hash[ptc].each do |dept|
+              if dept.blank? # Verifica se ha alguma Unidade Organizacional em branco quando a condição diz que não Existe Vinculo Institucional
+                errors.add(:base, "Unidade Organizacional sem Vinculo Institucional em '#{PersonType.find(ptc).description}'") # => Adiciona o erro
+                validator = false # => retorna falso se nao houver Unidade Organizacional selecionada mas sem o Vinculo Institucional estiver selecionado
+                break
+              end
+            end
           end
         end
       end
-      validator
+      validator #Retorna Verdadeiro ou falso dependendo das condições acima
     end  
   end
   
